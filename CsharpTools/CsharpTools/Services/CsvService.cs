762628f0e7d6d778898177d2ee7ac611322534ea
Thomas BERNARD <tbernard@pageup.fr>
2022-04-08 11:43:19 +0200

    add csvService

---
+++ CsharpTools/CsharpTools/Services/CsvService.cs
@@ -0,0 +1,17 @@
+﻿using CsharpTools.Services.Interfaces;
+
+namespace CsharpTools.Services
+{
+    public class CsvService : ICsvService
+    {
+        public string CreateAndWrite<T>(IEnumerable<T> content)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<IEnumerable<string>> Read(string filePath)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
