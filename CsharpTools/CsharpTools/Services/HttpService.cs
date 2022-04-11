c2fb45623bc7f45c77d8114f417c35c4d7843f55
Thomas BERNARD <tbernard@pageup.fr>
2022-04-11 07:43:42 +0200

    Add interface for HttpService

--- CsharpTools/CsharpTools/Services/HttpService.cs
+++ CsharpTools/CsharpTools/Services/HttpService.cs
@@ -1,11 +1,12 @@
 ﻿using CsharpTools.Models;
+using CsharpTools.Services.Interfaces;
 using Newtonsoft.Json;
 using System.Net.Http.Headers;
 using System.Net.Http.Json;
 
 namespace CsharpTools.Services
 {
-    public class HttpService
+    public class HttpService : IHttpService
     {
         private readonly HttpClient _httpClient;
 
@@ -13,16 +14,7 @@ namespace CsharpTools.Services
         {
             _httpClient = new HttpClient();
         }
-
-        /// <summary>
-        /// This method allows to send Http requests on a url
-        /// </summary>
-        /// <typeparam name="T">How the content of the response should be deserialized</typeparam>
-        /// <param name="url">The address to send the request to</param>
-        /// <param name="httpMethod">The Http method (HttpMethod.Get, HttpMethod.Post...)</param>
-        /// <param name="body">The body to send (it will then be serialized)</param>
-        /// <param name="bearer">The bearer token</param>
-        /// <returns>This method returns an HttpResult, containing information about the request and the result</returns>
+        
         public async Task<HttpResult<T>> SendHttpRequest<T>(string url, HttpMethod httpMethod, object body = null, string bearer = "")
         {
             var httpResult = new HttpResult<T>();
