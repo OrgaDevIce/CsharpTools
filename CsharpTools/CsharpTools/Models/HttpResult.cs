b9a9ed16679029142df7b4fda5950d694d294572
Thomas BERNARD <tbernard@pageup.fr>
2022-04-11 07:50:45 +0200

    add HttpResult class without content

--- CsharpTools/CsharpTools/Models/HttpResult.cs
+++ CsharpTools/CsharpTools/Models/HttpResult.cs
@@ -2,22 +2,34 @@
 
 namespace CsharpTools.Models
 {
-    public class HttpResult<T>
+    public class HttpResult
     {
         public string ErrorMessage { get; set; }
-        public T Content { get; set; }
         public HttpStatusCode Status { get; set; }
         public HttpRequestMessage RequestMessage { get; set; }
 
         public HttpResult()
         {
-
         }
 
         public HttpResult(HttpResponseMessage httpResponseMessage)
         {
             RequestMessage = httpResponseMessage.RequestMessage;
             Status = httpResponseMessage.StatusCode;
+        }
+    }
+
+
+    public class HttpResult<T> : HttpResult
+    {
+        public T Content { get; set; }
+
+        public HttpResult()
+        {
+        }
+
+        public HttpResult(HttpResponseMessage httpResponseMessage):base(httpResponseMessage)
+        {
         }
     }
 }
