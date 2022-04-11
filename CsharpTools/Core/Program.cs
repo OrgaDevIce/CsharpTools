87846dc295032b38fefc1d096a2c343fea98b01e
Thomas BERNARD <tbernard@pageup.fr>
2022-04-11 08:29:38 +0200

    test http method

--- CsharpTools/Core/Program.cs
+++ CsharpTools/Core/Program.cs
@@ -1,4 +1,13 @@
-﻿using CsharpTools.Services;
+﻿using Core.Models;
+using CsharpTools.Services;
+using CsharpTools.Services.Interfaces;
 
-var httpService = new HttpService();
+IHttpService httpService = new HttpService();
 var csvService = new CsvService();
+
+var url = "https://www.thecolorapi.com/scheme?hex=222222";
+
+var res = await httpService.SendHttpRequest(url, HttpMethod.Get);
+var res2 = await httpService.SendHttpRequest<Root>(url, HttpMethod.Get);
+
+Console.ReadLine();
