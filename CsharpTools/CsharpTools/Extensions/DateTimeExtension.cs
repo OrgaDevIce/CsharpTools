12dfb2b465ede889c68f4bbaf9bac8fcc6be26e7
Thomas BERNARD <tbernard@pageup.fr>
2022-05-10 08:56:39 +0200

    Create GetFirstDayOfWeek

--- CsharpTools/CsharpTools/Extensions/DateTimeExtension.cs
+++ CsharpTools/CsharpTools/Extensions/DateTimeExtension.cs
@@ -13,5 +13,20 @@
         {
             return dateTime.Ticks >= rangeBeggin.Ticks && dateTime.Ticks <= rangeEnd.Ticks;
         }
+
+        /// <summary>
+        /// Get the first day of the week of datetime (first day of the week => Monday)
+        /// </summary>
+        /// <param name="dateTime">The current datetime which would the first day of the week</param>
+        /// <returns>The first day of the week</returns>
+        public static DateTime GetFirstDayOfWeek(this DateTime dateTime)
+        {
+            var dayNumber = (int)dateTime.DayOfWeek;
+            dayNumber = dayNumber == 0 ? 7 : dayNumber;
+
+            var firstDayOfWeek = dateTime.AddDays(-1 * (dayNumber - 1));
+
+            return firstDayOfWeek;
+        }
     }
 }
