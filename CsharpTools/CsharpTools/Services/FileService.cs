22df62cef41e8dc6fde5eb53cd17cf081eb1d86e
Thomas BERNARD <tbernard@pageup.fr>
2022-05-10 11:51:49 +0200

    Create adn developp FileService

---
+++ CsharpTools/CsharpTools/Services/FileService.cs
@@ -0,0 +1,37 @@
+﻿using CsharpTools.Services.Interfaces;
+
+namespace CsharpTools.Services
+{
+    public class FileService : IFileService
+    {
+        public string CreateFile(string path, string name)
+        {
+            var stream = File.Create(path + "\\" + name);
+
+            return stream.Name;
+        }
+
+        public bool PathExist(string path)
+        {
+            return new DirectoryInfo(path).Exists;
+        }
+
+        public IEnumerable<string> ReadFile(string path)
+        {
+            return File.ReadAllLines(path);
+        }
+
+        public bool RemoveFile(string path)
+        {
+            try
+            {
+                File.Delete(path);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
