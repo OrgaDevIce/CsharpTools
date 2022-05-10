0cb2a21801a35ad9e5ef169aace28c2170f9112e
Thomas BERNARD <tbernard@pageup.fr>
2022-05-10 07:53:49 +0200

    Add IntExtension

--- CsharpTools/CsharpTools/Extensions/IntExtension.cs
+++ CsharpTools/CsharpTools/Extensions/IntExtension.cs
@@ -1,6 +1,12 @@
 ﻿namespace CsharpTools.Extensions
 {
-    public static class IntExtension
+    public partial class IntExtension
     {
+        private static readonly Random _random = new Random();
+
+        public static int Random(int minValue, int maxValue)
+        {
+            return _random.Next(minValue, maxValue);
+        }
     }
 }
