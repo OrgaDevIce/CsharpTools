34a438caf51cd459f1b48bdd9ef85bbfb846bc8b
Thomas BERNARD <tbernard@pageup.fr>
2022-05-10 07:58:58 +0200

    Add comment to IntExtension.Random and test it

--- CsharpTools/CsharpTools/Extensions/IntExtension.cs
+++ CsharpTools/CsharpTools/Extensions/IntExtension.cs
@@ -4,9 +4,15 @@
     {
         private static readonly Random _random = new Random();
 
+        /// <summary>
+        /// This method generate a random int value
+        /// </summary>
+        /// <param name="minValue">Minimum value (included)</param>
+        /// <param name="maxValue">Maximum value (included)</param>
+        /// <returns>A random int between minValue (included) and maxValue (included)</returns>
         public static int Random(int minValue, int maxValue)
         {
-            return _random.Next(minValue, maxValue);
+            return _random.Next(minValue, maxValue + 1);
         }
     }
 }
