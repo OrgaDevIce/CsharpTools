7f6c19b373fa7a2b7eda315a4a706fcc7a157584
Thomas Bernard <tbernard@pageup.fr>
2022-05-27 13:29:55 +0200

    Change namespace + comments

--- CsharpTools/CsharpTools/Extensions/IntExtension.cs
+++ CsharpTools/CsharpTools/Extensions/IntExtension.cs
@@ -1,23 +1,28 @@
-﻿namespace CsharpTools.Extensions
+﻿namespace CsharpTools.Extensions;
+
+public static class IntExtension
 {
-    public static class IntExtension
+    private static readonly Random _random = new Random();
+
+    /// <summary>
+    /// This method generate a random int value
+    /// </summary>
+    /// <param name="minValue">Minimum value (included)</param>
+    /// <param name="maxValue">Maximum value (included)</param>
+    /// <returns>A random int between minValue (included) and maxValue (included)</returns>
+    public static int Random(int minValue, int maxValue)
     {
-        private static readonly Random _random = new Random();
+        return _random.Next(minValue, maxValue + 1);
+    }
 
-        /// <summary>
-        /// This method generate a random int value
-        /// </summary>
-        /// <param name="minValue">Minimum value (included)</param>
-        /// <param name="maxValue">Maximum value (included)</param>
-        /// <returns>A random int between minValue (included) and maxValue (included)</returns>
-        public static int Random(int minValue, int maxValue)
-        {
-            return _random.Next(minValue, maxValue + 1);
-        }
-
-        public static bool IsNull(this int value)
-        {
-            return value == 0 || value == null;
-        }
+    /// <summary>
+    /// Return true if value is null or equal 0
+    /// </summary>
+    /// <param name="value"></param>
+    /// <returns>True if null or equal 0</returns>
+    public static bool IsNullOrZero(this int? value)
+    {
+        return value == 0 || value == null;
     }
 }
+
